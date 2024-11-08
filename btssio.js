f8483f89026d6d56760e80cdeb85366cb145ea00
Swonath <lanathaud@gmail.com>
2024-11-08 21:11:45 +0100

    08/11/2024

---
+++ btssio.js
@@ -0,0 +1,20 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const btnSISR = document.getElementById("btnSISR");
+    const btnSLAM = document.getElementById("btnSLAM");
+    const contentSISR = document.getElementById("contentSISR");
+    const contentSLAM = document.getElementById("contentSLAM");
+
+    btnSISR.addEventListener("click", function() {
+        toggleContent(contentSISR, btnSISR);
+    });
+
+    btnSLAM.addEventListener("click", function() {
+        toggleContent(contentSLAM, btnSLAM);
+    });
+
+    function toggleContent(contentElement, button) {
+        contentElement.classList.toggle("visible");
+        button.classList.toggle("active");
+    }
+});
